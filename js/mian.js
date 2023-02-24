4b10afcf84b26686dd4870b905253cb019824baa
ahmed-samir <ahmedsamirr126@gmail.com>
2023-02-24 19:23:47 +0200

    create btn to up

--- js/mian.js
+++ js/mian.js
@@ -5,3 +5,29 @@ menu.onclick = () => {
     nav.classList.toggle("open")
     menu.classList.toggle("close")
 }
+
+// btn to up
+
+let btn = document.querySelector('.btn-up')
+let header = document.querySelector('header')
+
+// show btn
+window.onscroll = function () {
+    if (window.scrollY >= 200) {
+        btn.style.display = 'block'
+        header.style.backgroundColor = 'rgba(5, 44, 63, 0.8)'
+
+    } else {
+        btn.style.display = 'none'
+        header.style.backgroundColor = 'transparent'
+
+    }
+}
+
+// move to up 
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
