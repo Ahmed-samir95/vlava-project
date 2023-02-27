8ddd0ecfebd307a43e8de610156d98216ef6d82f
ahmed-samir <ahmedsamirr126@gmail.com>
2023-02-27 20:11:27 +0200

    Complete the Portfolio section

--- js/mian.js
+++ js/mian.js
@@ -1,3 +1,4 @@
+// show nav
 let menu = document.querySelector('.menu-icon')
 let nav = document.querySelector('.nav')
 console.log(nav)
@@ -31,3 +32,41 @@ btn.onclick = function () {
         behavior: "smooth"
     })
 }
+
+// portfolio filter
+let filterBtns = document.querySelector('#filter-btns').children
+let portfolioContent = document.querySelector('.portfolio-content').children
+
+for (let i = 0; i < filterBtns.length; i++) {
+    filterBtns[i].addEventListener("click", function() {
+        for (let j = 0; j < filterBtns.length; j++) {
+            filterBtns[j].classList.remove('active')
+        }
+        this.classList.add('active')
+        let target = this.getAttribute('data-target')
+
+        for (let k = 0; k < portfolioContent.length; k++) {
+            portfolioContent[k].style.display = 'none'
+            if (target == portfolioContent[k].getAttribute('data-id')) {
+                portfolioContent[k].style.display = 'block'
+            }
+        }
+        if (target == 'all') {
+            portfolioContent[k].style.display = 'block'
+        }
+    })
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
